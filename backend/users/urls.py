ad898adc2368d50ffcd98c7f291bc0942862eb53
Paweł Jurek <pawel80121@gmail.com>
2024-04-10 19:01:31 +0200

    Item model connected to Offer

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,5 +4,5 @@ from .views import get_user, create_user, edit_user
 urlpatterns = [
     path('<str:steam_id>/', get_user, name='get_user_by_steam_id'),
     path('', create_user, name='create_user'),
-    path('edit/<str:steam_id>', user_edit, name='user-edit')
+    path('edit/<str:steam_id>', edit_user, name='edit_user')
 ]
