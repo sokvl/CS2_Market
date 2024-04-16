fe68bfe3e3fe8310e1e02f179cb763e3ba49b331
Paweł Jurek <pawel80121@gmail.com>
2024-04-16 16:23:26 +0200

    Untested endpoints for notifications

--- backend/api/serializers/User_serializer.py
+++ backend/api/serializers/User_serializer.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/offers/urls.py
+++ backend/offers/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import get_offer, get_item, create_item, create_offer
 
 urlpatterns = [
-    path('<str:offer_id>/', get_offer, name='get_offer_by_id'),
-    path('create', create_item, name='create_offer'),
+    path('create', create_offer, name='create_offer'),
+    path('<str:offer_id>/', get_offer, name='get_offer_by_id'),   
     ## ITEM
     path('create_item', create_item, name='create_item'),
     path('item/<str:item_id>', get_item, name='get_item')
