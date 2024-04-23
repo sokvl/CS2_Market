458c1536668860ef42e594fcbd0ce9357a98b1bb
sokvl <msokvl@gmail.com>
2024-04-23 20:20:13 +0200

    Payment system setup(phase 1/2)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views.offers_views import testget
+from .views.payment_views import create_checkout_session
 from .views.auth_views import steam_login, steam_login_callback
 
 urlpatterns = [
     path('login/', steam_login),
-    path('callback/', steam_login_callback)
+    path('callback/', steam_login_callback),
+    path('payment/', create_checkout_session)
 ]
--- backend/transactions/urls.py
+++ backend/transactions/urls.py
@@ -4,5 +4,4 @@ from .views import get_notification, create_notification
 urlpatterns = [
     path('create_notification/', create_notification, name='create_notification'),
     path('notification/<int:notification_id>/', get_notification, name='get_notification_by_id'),
-    
 ]
