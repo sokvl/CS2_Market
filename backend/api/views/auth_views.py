1e9137d5c0e2f9a47f50e3ae8306e74b1216ae22
sokvl <msokvl@gmail.com>
2024-04-24 19:23:20 +0200

    User code refactoring, setup for JWTauthentication

--- backend/api/views/auth_views.py
+++ backend/api/views/auth_views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from steamauth import auth, get_uid
 from api.serializers.User_serializer import UserSerializer
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 
 
 import os
@@ -17,7 +17,6 @@ def steam_login_callback(request):
         return redirect('/failed')
     try:
         db_user = get_user_model().objects.get(steam_id=user)
-        print("ID:", db_user)
     except get_user_model().DoesNotExist:
         STEAMAPI_KEY = os.getenv('STEAMAPI_KEY')
         response = requests.get(
@@ -37,8 +36,11 @@ def steam_login_callback(request):
         })
         if serializer.is_valid():
             serializer.save()
+            login(request, db_user)
             return redirect(f'/users/{user}')
-        
-        return redirect('/fail')
+    if(db_user):
+        login(request, db_user)
+        return redirect(f'/users/{user}')
+
 
         
