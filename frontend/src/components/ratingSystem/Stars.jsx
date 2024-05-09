a9e32434d01c2f6ebc183880517725844b72ad95
1SHIGERU <pat-hur@wp.pl>
2024-05-09 12:30:33 +0200

    wystylizowany admin panel, trzeba podlaczyc

---
+++ frontend/src/components/ratingSystem/Stars.jsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'
+import {useTheme} from '../../ThemeContext';
+import './stars.css'
+
+const Stars = () => {
+
+    return (
+        
+            <form class="rating">
+                <label>
+                    <input type="radio" name="stars" value="1" />
+                    <span class="icon">★</span>
+                </label>
+                <label>
+                    <input type="radio" name="stars" value="2" />
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>
+                </label>
+                <label>
+                    <input type="radio" name="stars" value="3" />
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>   
+                </label>
+                <label>
+                    <input type="radio" name="stars" value="4" />
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>
+                </label>
+                <label>
+                    <input type="radio" name="stars" value="5" />
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>
+                    <span class="icon">★</span>
+                </label>
+            </form>    
+        
+    );
+}
+export default Stars
