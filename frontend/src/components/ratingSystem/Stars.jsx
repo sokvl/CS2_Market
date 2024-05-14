cfbaec17d6998b4d6b7f14b62af9480e8d056855
1SHIGERU <pat-hur@wp.pl>
2024-05-14 23:09:54 +0200

    podlaczony i wystylizowany raport 1, feedback tez dziala i ladnie wyglada pozdrooo

--- frontend/src/components/ratingSystem/Stars.jsx
+++ frontend/src/components/ratingSystem/Stars.jsx
@@ -2,35 +2,45 @@ import React, {useState, useEffect} from 'react'
 import {useTheme} from '../../ThemeContext';
 import './stars.css'
 
-const Stars = () => {
+const Stars = ({ onStarChange }) => {
+
+    const [selectedStars, setSelectedStars] = useState(0);
+
+    const handleStarChange = (event) => {
+        const stars = parseInt(event.target.value);
+        setSelectedStars(stars);
+        if (onStarChange) {
+          onStarChange(stars);
+        }
+      };
 
     return (
         
             <form class="rating">
                 <label>
-                    <input type="radio" name="stars" value="1" />
+                    <input type="radio" name="stars" value="1" onChange={handleStarChange}/>
                     <span class="icon">★</span>
                 </label>
                 <label>
-                    <input type="radio" name="stars" value="2" />
+                    <input type="radio" name="stars" value="2" onChange={handleStarChange}/>
                     <span class="icon">★</span>
                     <span class="icon">★</span>
                 </label>
                 <label>
-                    <input type="radio" name="stars" value="3" />
+                    <input type="radio" name="stars" value="3" onChange={handleStarChange}/>
                     <span class="icon">★</span>
                     <span class="icon">★</span>
                     <span class="icon">★</span>   
                 </label>
                 <label>
-                    <input type="radio" name="stars" value="4" />
+                    <input type="radio" name="stars" value="4" onChange={handleStarChange}/>
                     <span class="icon">★</span>
                     <span class="icon">★</span>
                     <span class="icon">★</span>
                     <span class="icon">★</span>
                 </label>
                 <label>
-                    <input type="radio" name="stars" value="5" />
+                    <input type="radio" name="stars" value="5" onChange={handleStarChange}/>
                     <span class="icon">★</span>
                     <span class="icon">★</span>
                     <span class="icon">★</span>
