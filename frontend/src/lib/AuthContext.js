febce2789c7d32b24c87f80a57743a8db5fa23d3
sokvl <msokvl@gmail.com>
2024-05-15 14:28:48 +0200

    Auth finalization

--- frontend/src/lib/AuthContext.js
+++ frontend/src/lib/AuthContext.js
@@ -10,7 +10,7 @@ export default AuthContext;
 export const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
-    const [authTokens, setAuthTokens] = useState(null)
+    const [authTokens, setAuthTokens] = useState(null);
 
     let loginUser = async () => {
         const tempTokens = {
@@ -55,6 +55,16 @@ export const AuthProvider = ({children}) => {
     }
 
     useEffect(()=> {
+
+        let tokens = { 
+            access: localStorage.getItem("access"),
+            refresh: localStorage.getItem("refresh")
+        }
+
+        if(tokens.access != null) {
+            setAuthTokens(tokens)
+            setUser(jwtDecode(tokens.access.toString()))
+        }
 
         let timeInterval = 270000
 
