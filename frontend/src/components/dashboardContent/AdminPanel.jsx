4377b94d8a7d53863a390beab693b81fd1353bad
1SHIGERU <pat-hur@wp.pl>
2024-05-20 15:55:05 +0200

    podpięty raport2, dziala, nie wywraca sie, jest zabezpieczony przed impostorami, ale brak jeszcze feedbacku (pozniej zrobie)

--- frontend/src/components/dashboardContent/AdminPanel.jsx
+++ frontend/src/components/dashboardContent/AdminPanel.jsx
@@ -9,29 +9,64 @@ const AdminPanel = ({steamid}) => {
 
     const [showPopup, setShowPopup] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
+    const [raport1, setRaport1] = useState(false);
+    const [raport2, setRaport2] = useState(false);
 
     const [selectedOption, setSelectedOption] = useState('option1');
-    const [activeView, setActiveView] = useState('view1');
 
     const [selectedStars, setSelectedStars] = useState(0);
     const [selectedStars2, setSelectedStars2] = useState(0);
 
-    const [data, setData] = useState([]);
-    
+    const [raport1Data, setRaport1Data] = useState([]);
+    const [raport2Data, setRaport2Data] = useState([]);
+
+    const [priceMin, setPriceMin] = useState(0);
+    const [priceMax, setPriceMax] = useState(0);
+
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+
+    const [selectedCategory, setSelectedCategory] = useState('all');
+ 
     const handleStarChange = (stars, whichStar) => {
       if (whichStar === 1) setSelectedStars(stars);
       else setSelectedStars2(stars);
     };
 
     const handleRadioChange = (event) => {
+        if(event.target.value === 'option1') {
+          setRaport1(true);
+          setRaport2(false);
+        } 
+        if(event.target.value === 'option2') {
+          setRaport2(true);
+          setRaport1(false);
+        }
         setSelectedOption(event.target.value);
+        
     };
 
-    const handleViewChange = (event) => {
-        setActiveView(event.target.value);
+    const handleMinPriceChange = (event) => {
+      setPriceMin(event.target.value);
+    };
+  
+    const handleMaxPriceChange = (event) => {
+      setPriceMax(event.target.value);
     };
 
-    const handleRaportGenerate = () => {
+    const handleStartDateChange = (event) => {
+      setStartDate(event.target.value);
+    };
+  
+    const handleEndDateChange = (event) => {
+      setEndDate(event.target.value);
+    };
+
+    const handleCategoryChange = (event) => {
+      setSelectedCategory(event.target.value);
+    };
+
+    const handleRaport1Generate = () => {
       if(selectedStars === 0 || selectedStars2 === 0) {
         alert('Wybierz zakres ocen');
         return;
@@ -44,18 +79,50 @@ const AdminPanel = ({steamid}) => {
         axios.get(`http://localhost:8000/reports/rating/?min_rating=${selectedStars}&max_rating=${selectedStars2}`)
        .then((response) => {
           
-          setData(response.data);
+          setRaport1Data(response.data);
           setShowPopup(true);
           setIsLoading(true);
           setTimeout(() => {
             setIsLoading(false);
             
-          }, 3000); 
+          }, 2000); 
           
        })
        .catch((error) => {
          console.error('Error fetching data:', error);
        });
+
+    }
+
+    const handleRaport2Generate = () => {
+
+      if(priceMin === 0 || priceMax === 0) {
+        alert('Podaj zakres cen');
+        return;
+      }
+      if(priceMin > priceMax || priceMin < 0 || priceMax < 0) {
+        alert('Zakres cen jest niepoprawny');
+        return;
+      }
+
+      if(startDate === '' || endDate === '') {
+        alert('Podaj zakres dat');
+        return;
+      }
+
+      axios.get(`http://localhost:8000/reports/transaction/?min_price=${priceMin}&max_price=${priceMax}&start_date=${startDate}&end_date=${endDate}&category=${selectedCategory}`)
+      .then((response) => {
+
+        setRaport2Data(response.data);
+        setShowPopup(true);
+        setIsLoading(true);
+        setTimeout(() => {
+          setIsLoading(false);    
+        }, 2000); 
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
 
     }
 
@@ -74,7 +141,7 @@ const AdminPanel = ({steamid}) => {
                   <p>Do: </p>
                   <Stars onStarChange={(stars) => handleStarChange(stars, 2)} />
                 </div>
-                <button onClick={handleRaportGenerate} className={`font-bold py-2 px-4 rounded mt-4 ${isDarkMode ? 'bg-[#242633]' : 'bg-blue-500 text-white hover:bg-blue-700'}`}>
+                <button onClick={handleRaport1Generate} className={`font-bold py-2 px-4 rounded mt-4 ${isDarkMode ? 'bg-[#242633]' : 'bg-blue-500 text-white hover:bg-blue-700'}`}>
                   Wygeneruj raport
                 </button>
               </div>
@@ -85,51 +152,53 @@ const AdminPanel = ({steamid}) => {
             <div className="flex flex-col items-center justify-center">
                 <p className="mt-8 text-2xl text-center">Raport 2</p><br/>
                 <form class="block w-96 items-center justify-center shadow-lg p-4 rounded-xl">
-
                     <div class="grid grid-cols-2 gap-4">
                         <div class="relative z-0 w-full mb-5">
-                        <input type="text" name="dd" placeholder="price min:" class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-600 border-gray-200 font-sans" />
+                        <input onChange={handleMinPriceChange} type="number" name="dd" placeholder="price min:" class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-600 border-gray-200 font-sans" />
                         <label for="dd" class="absolute duration-200 top-3 -z-1 origin-0 text-gray-500 text-base"></label>
                         </div>
                         <div class="relative z-0 w-full mb-5">
-                        <input type="text" name="mm" placeholder="price max:" class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-600 border-gray-200 font-sans" />
+                        <input onChange={handleMaxPriceChange} type="number" name="mm" placeholder="price max:" class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-600 border-gray-200 font-sans" />
                         <label for="mm" class="absolute duration-200 top-3 -z-1 origin-0 text-gray-500 text-base"></label>
                         </div>
                     </div>
 
                     <div class="grid grid-cols-2 gap-4">
                         <div class="relative z-0 w-full mb-5">
-                        <input type="date" name="dd" placeholder="od: " class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-600 border-gray-200 font-sans" />
+                        <input onChange={handleStartDateChange} type="date" name="dd" placeholder="od: " class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-600 border-gray-200 font-sans" />
                         <label for="dd" class="absolute duration-200 top-3 -z-1 origin-0 text-gray-500 text-base"></label>
                         </div>
                         <div class="relative z-0 w-full mb-5">
-                        <input type="date" name="mm" placeholder="do: " class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-600 border-gray-200 font-sans" />
+                        <input onChange={handleEndDateChange} type="date" name="mm" placeholder="do: " class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-600 border-gray-200 font-sans" />
                         <label for="mm" class="absolute duration-200 top-3 -z-1 origin-0 text-gray-500 text-base"></label>
                         </div>
                     </div>
                     
                     <div class="relative z-0 w-full mb-5">
-
-                        <select id="category" name="category" class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:bg-transparent focus:border-indigo-600 border-gray-200 font-sans">
-                            <option value="" defaultChecked>Pick category</option>
-                            <option value="all" >All</option>
-                            <option value="food">Knives</option>
-                            <option value="clothing">Rifles</option>
-                            <option value="electronics">Sniper Rifles</option>
-                            <option value="electronics">Pistols </option>
-                            <option value="electronics">SMG </option>
-                            <option value="electronics">Shotguns </option>
-                            <option value="electronics">Machine Guns </option>
-                            <option value="electronics">Containers </option>
-                            <option value="electronics">Gloves </option>
-                            <option value="electronics">Agents </option>
-                        </select>
-                    </div>
-                    
-                    <button className={`font-bold py-2 px-4 rounded mt-4 ${isDarkMode ? 'bg-[#242633]' : 'bg-blue-500 text-white hover:bg-blue-700'}`}>
-                        Wygeneruj raport
-                    </button>
+                    <select
+                      id="category"
+                      name="category"
+                      className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:bg-transparent focus:border-indigo-600 border-gray-200 font-sans"
+                      value={selectedCategory}
+                      onChange={handleCategoryChange}
+                    >
+                      <option className="text-black" value="all" defaultChecked>All</option>
+                      <option className="text-black" value="knives">Knives</option>
+                      <option className="text-black" value="rifles">Rifles</option>
+                      <option className="text-black" value="sniper_rifles">Sniper Rifles</option>
+                      <option className="text-black" value="pistols">Pistols</option>
+                      <option className="text-black" value="smg">SMG</option>
+                      <option className="text-black" value="shotguns">Shotguns</option>
+                      <option className="text-black" value="machine_guns">Machine Guns</option>
+                      <option className="text-black" value="containers">Containers</option>
+                      <option className="text-black" value="gloves">Gloves</option>
+                      <option className="text-black" value="agents">Agents</option>
+                    </select>
+                    </div>              
                 </form>
+                <button onClick={handleRaport2Generate} className={` font-bold py-2 px-4 rounded mt-4 ${isDarkMode ? 'bg-[#242633]' : 'bg-blue-500 text-white hover:bg-blue-700'}`}>
+                  Wygeneruj raport
+                </button>
             </div>
           );
         }
@@ -143,47 +212,69 @@ const AdminPanel = ({steamid}) => {
          <div className={`${isDarkMode ? 'bg-[#1f1d24]' : 'bg-gradient-to-r from-blue-800 to-blue-900 text-white' } p-6 rounded-xl mt-2 h-full md:w-2/2 md:ml-6'`}>
             <p className='mb-4 text-3xl text-center'> Admin Panel </p>
 
-            {showPopup && (
+            {showPopup && raport1 ? (
               <>
                 <div className="overlay" onClick={() => setShowPopup(false)}></div>
-                  <div className="popUp">
-                    {isLoading ? (
-                      <>
-                        <h1 className='text-3xl text-center'> Raport is generating, wait a second please ... </h1>
-                        <div className="loading">Loading&#8230;</div> 
-                      </>
-                    ) : (
-                      <>
-                        <h1 className='text-3xl text-center mb-4'> Raport results</h1>
-                        <p className='text-xl text-center'>Users with average rating between {selectedStars} and {selectedStars2} <i className="fa-solid fa-star "></i></p>
-                        <table className="mt-8 w-full min-w-max table-auto text-left">
-                          <thead>
-                            <tr className='text-xl'>
-                              <th className="text-center">#</th>
-                              <th className="text-center">avatar</th>
-                              <th className="text-center">username</th>
-                              <th className="text-center">avg rating</th>
-                              <th className="text-center">no rates</th>
+                <div className="popUp">
+                  {isLoading ? (
+                    <>
+                      <h1 className='text-3xl text-center'> Raport is generating, wait a second please ... </h1>
+                      <div className="loading">Loading&#8230;</div>
+                    </>
+                  ) : (
+                    <>
+                      <h1 className='text-3xl text-center mb-4'> Raport results</h1>
+                      <p className='text-xl text-center'>Users with average rating between {selectedStars} and {selectedStars2} <i className="fa-solid fa-star "></i></p>
+                      <table className="mt-8 w-full min-w-max table-auto text-left">
+                        <thead>
+                          <tr className='text-xl'>
+                            <th className="text-center">#</th>
+                            <th className="text-center">avatar</th>
+                            <th className="text-center">username</th>
+                            <th className="text-center">avg rating</th>
+                            <th className="text-center">no rates</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {raport1Data.map((user, i) => (
+                            <tr key={i}>
+                              <td className="text-center">{i+1}</td>
+                              <td className="text-center"><img src={user.avatar_url} alt="avatar" className="w-10 h-10 rounded-full mx-auto" /></td>
+                              <td className="text-center">{user.username}</td>
+                              <td className="text-center">{user.average_rating}</td>
+                              <td className="text-center">{user.number_of_ratings}</td>
                             </tr>
-                          </thead>
-                              {data.map((user, i) => (
-                                <tbody key={i}>
-                                  <tr>
-                                    <td className="text-center">{i+1}</td>
-                                    <td className="text-center"><img src={user.avatar_url} alt="avatar" className="w-10 h-10 rounded-full mx-auto" /></td>
-                                    <td className="text-center">{user.username}</td>
-                                    <td className="text-center">{user.average_rating}</td>
-                                    <td className="text-center">{user.number_of_ratings}</td>
-                                  </tr>
-                                </tbody>
-                              ))}
-                        </table>
-                      </>
-                    )}
-
-                  </div>
+                          ))}
+                        </tbody>
+                      </table>
+                    </>
+                  )}
+                </div>
               </>
+            ) : (
+              <></>
             )}
+
+
+              {showPopup && !raport1 ? (
+                            <>
+                              <div className="overlay" onClick={() => setShowPopup(false)}></div>
+                              <div className="popUp">
+                                {isLoading ? (
+                                  <>
+                                    <h1 className='text-3xl text-center'> Raport2 is generating, wait a second please ... </h1>
+                                    <div className="loading">Loading&#8230;</div>
+                                  </>
+                                ) : (
+                                  <>
+                                    <p className='text-3xl text-center mb-4'> Raport2 results</p>
+                                  </>
+                                )}
+                              </div>
+                            </>
+                          ) : (
+                            <></>
+                          )}
 
 
             <div className="grid w-[20rem] grid-cols-2 gap-2 rounded-xl text-black bg-gray-200 p-2 mx-auto items-center justify-center">
