1d9e9da12e1ad3e95f397ead6064b0a2f3e9672b
Paweł Jurek <pawel80121@gmail.com>
2024-05-30 16:29:17 +0200

    user active offers

--- backend/offers/urls.py
+++ backend/offers/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ItemDetailView, OfferViewSet
+from .views import ItemDetailView, OfferViewSet, UserActiveOffersView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -7,5 +7,6 @@ router.register(r'', OfferViewSet)
 
 urlpatterns = [
     path('item/<int:item_id>/', ItemDetailView.as_view(), name='item-detail'),
+    path('user-active/', UserActiveOffersView.as_view(), name='user-active-offers'),
     path('', include(router.urls)),
 ]
